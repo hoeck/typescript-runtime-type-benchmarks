08dfaf1c84aaac5676edf08ee5f1a7c4c87dc36e
Erik Soehnel <erik.soehnel@gmail.com>
2024-09-15 12:55:57 +0200

    basic Vite + React setup
    With prettier and strict ESlint rules.

---
+++ app/vite.config.ts
@@ -0,0 +1,9 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  // base dir for gh-pages
+  base: "typescript-runtime-time-benchmarks/",
+  plugins: [react()],
+});
